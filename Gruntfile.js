7b28eaa6e33d7ed52af2e4ca12bd4883fe8db120
thomaswilburn <twilburn@npr.org>
2019-12-06 13:38:59 -0600

    Name the local task accordingly

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,6 @@ module.exports = function(grunt) {
   ]);
   grunt.registerTask("serve", "Start the dev server", ["connect:dev", "watch"]);
   grunt.registerTask("default", ["clean", "static", "serve"]);
-  grunt.registerTask("start", "Begin a local cron cycle", ["connect:dev", "cron:10"]);
-  grunt.registerTask("deploy", "Start deploying on a timer", ["cron:15:publish"]);
+  grunt.registerTask("local", "Begin a local cron cycle", ["static", "connect:dev", "cron:10"]);
+  grunt.registerTask("deploy", "Start deploying on a timer", ["static", "cron:15:publish"]);
 };
