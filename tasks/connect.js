bbabdc3835524b748b630a5102534074736afb31
thomaswilburn <twilburn@npr.org>
2019-12-09 10:59:45 -0600

    Remove forever, add systemd service def
    Will need to test this on the server, I suspect we'll have to use a different method of running grunt.

--- tasks/connect.js
+++ tasks/connect.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
     connect: {
       dev: {
         options: {
-          hostname: "localhost",
+          // hostname: "localhost",
           useAvailablePort: true,
           port: grunt.option("port") || 8000,
           livereload: grunt.option("reloadport") * 1 || 35739,
