07205b94531f147045344df03859a9f170c93ab0
thomaswilburn <twilburn@npr.org>
2019-12-30 11:09:39 -0600

    Loading for corepub screwiness
    Since side-chain gets stripped, automatically create those elements dynamically from paragraphs with data-sidechain-src attributes.

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,6 +1,13 @@
+var $ = require("./lib/qsa");
+
 // load Sidechain and the legacy browser shim
 require("@webcomponents/custom-elements");
 var Sidechain = require("@nprapps/sidechain");
+
+// ugh CorePub
+$("[data-sidechain-src]").forEach(function(p) {
+  p.innerHTML = `<side-chain src="${p.dataset.sidechainSrc}"></side-chain>`;
+})
 
 // handle events from the inner frame
 var onMessage = Sidechain.matchMessage({
