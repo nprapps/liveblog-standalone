7a0bd0d9f8a93f2fe4b11b750e5568fbbf67bc8d
thomaswilburn <twilburn@npr.org>
2019-12-30 14:16:15 -0600

    Prep to test on the server

--- tasks/cron.js
+++ tasks/cron.js
@@ -7,7 +7,7 @@ Runs tasks on an automated basis
 /** config variables **/
 var tasks = {
   local: ["docs", "static"],
-  publish: ["docs", "quick", "publish:live"]
+  publish: ["docs", "quick", "publish"]
 };
 
 /** end config **/
