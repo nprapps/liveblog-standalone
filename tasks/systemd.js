12240ef7227ca3b8d18448bcfe5c1f66d72b7cdf
thomaswilburn <twilburn@npr.org>
2020-01-14 18:18:50 -0600

    .

--- tasks/systemd.js
+++ tasks/systemd.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
     var here = process.cwd();
 
     template = template.replace(/%HOME%/g, home);
-    template = template.replace(/%HERE%/g, home);
+    template = template.replace(/%HERE%/g, here);
     var envString = Object.keys(env).map(k => `Environment=${k}="${env[k]}"`).join("\n");
     template = template.replace("%ENV%", envString);
 
