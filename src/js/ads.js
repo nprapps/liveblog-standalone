c22252390079922ab23aa63e004bd12b25c091bb
thomaswilburn <twilburn@npr.org>
2020-01-17 13:59:34 -0600

    Make ads a custom element
    Allows us to have multiple ads if we feel like it. Currently only one shows up.
    See also #22

--- src/js/ads.js
+++ src/js/ads.js
@@ -1,46 +1,73 @@
 var $ = require("./lib/qsa");
-var adElement = $.one("#ad-centerstage-wrap");
+window.googletag = window.googletag || {cmd: []};
+var gptLoaded = false;
+var gptSetup = false;
+
+var slug = window.location.pathname.replace(/[^\/]+\.html$/, "").split("/").pop();
+var storyId = "liveblog-" + (slug || "localhost");
+var isStagingServer = true || window.location.hostname == "stage-apps.npr.org";
+var adSizeArray = ["fluid", [1300, 250]];
+var adUnitString = "/6735/n6735.npr/news_politics_elections";
+// Medium and small breakpoints
+if (window.innerWidth < 1024) {
+  adUnitString = "/6735/n6735.nprmobile/news_politics_elections";
+  adSizeArray.push([300, 250]);
+}
+var advelvetTargeting = [String(Math.floor(Math.random() * 20) + 1)];
 
 var observer = new IntersectionObserver(function([event]) {
   if (event.isIntersecting) {
     observer.disconnect();
     var gpt = document.createElement("script");
     gpt.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
+    gpt.async = true;
+    gpt.defer = true;
     document.body.appendChild(gpt);
     console.log("Lazy-loading ad code...");
+    gptLoaded = true;
   }
 });
 
-observer.observe(adElement);
+var guid = 0;
 
-window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function() {
-  var adSizeArray = ["fluid", [1300, 250]];
-  var adUnitString = "/6735/n6735.npr/news_politics_elections";
-  // Medium and small breakpoints
-  if (window.innerWidth < 1024) {
-    adUnitString = "/6735/n6735.nprmobile/news_politics_elections";
-    adSizeArray.push([300, 250]);
+class GoogleAd extends HTMLElement {
+  constructor() {
+    super();
+    if (!gptLoaded) observer.observe(this);
   }
-  var advelvetTargeting = [String(Math.floor(Math.random() * 20) + 1)];
-  var slug = window.location.pathname.replace(/[^\/]+\.html$/, "").split("/").pop();
-  var storyId = "liveblog-" + (slug || "localhost");
-  var isStagingServer = true || window.location.hostname == "stage-apps.npr.org";
 
-  googletag.defineSlot(adUnitString, adSizeArray, "ad-centerstage-wrap").addService(googletag.pubads());
-  googletag.pubads().enableSingleRequest();
-  googletag.pubads().collapseEmptyDivs();
-  googletag.pubads().setTargeting("advelvet", advelvetTargeting);
-  googletag.pubads().setTargeting("storyid", [storyId]);
-  googletag.pubads().setTargeting("testserver", [isStagingServer.toString()]);
-  googletag.pubads().addEventListener("slotRenderEnded", function(event) {
-    adElement.classList.remove("pending");
-    if (!event.isEmpty) {
-      adElement.classList.add("has-ad");
-    } else {
-      console.log("No ad returned");
-    }
-  });
-  googletag.enableServices();
-  googletag.display("ad-centerstage-wrap");
-});
+  connectedCallback() {
+    var id = this.getAttribute("id");
+    if (id) return;
+    id = "google-ad-" + guid++;
+    this.setAttribute("id", id);
+
+    googletag.cmd.push(() => {
+      var adService = googletag.pubads();
+
+      if (!gptSetup) {
+        adService.enableSingleRequest();
+        adService.collapseEmptyDivs();
+        adService.setTargeting("advelvet", advelvetTargeting);
+        adService.setTargeting("storyid", [storyId]);
+        adService.setTargeting("testserver", [isStagingServer.toString()]);
+        googletag.enableServices();
+        gptSetup = true;
+      }
+
+      googletag.defineSlot(adUnitString, adSizeArray, id).addService(adService);
+      adService.addEventListener("slotRenderEnded", event => {
+        if (event.slot.getSlotElementId() != id) return;
+        this.classList.remove("pending");
+        if (!event.isEmpty) {
+          this.classList.add("has-ad");
+        } else {
+          console.log(`No ad returned for ${id}`);
+        }
+      });
+      googletag.display(id);
+    });
+  }
+}
+
+window.customElements.define("google-ad", GoogleAd);
