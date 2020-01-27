cf34cbb09eb49bc616fe619068631facc56c929f
thomaswilburn <twilburn@npr.org>
2020-01-27 13:38:22 -0600

    Fix internal links for stations
    Related to #6

--- src/js/main-embedded.js
+++ src/js/main-embedded.js
@@ -15,3 +15,17 @@ events.on("unseen-posts", function(unseen) {
   guest.sendLegacy("update-parent-title", 0);
   guest.sendLegacy("update-parent-title", unseen);
 });
+
+//handle internal links
+document.body.addEventListener("click", function(e) {
+  var link = e.target.closest("a");
+  if (link) {
+    var href = link.getAttribute("href");
+    if (href[0] == "#") {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      var element = document.querySelector(href);
+      if (element) element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
