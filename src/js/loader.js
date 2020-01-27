74c01c01c3d83bd9516b125078d08a2b3fbc00de
thomaswilburn <twilburn@npr.org>
2020-01-26 22:05:40 -0500

    Add mutation observers for Sidechain upgrades
    Fix #13

--- src/js/loader.js
+++ src/js/loader.js
@@ -5,9 +5,26 @@ require("@webcomponents/custom-elements");
 var Sidechain = require("@nprapps/sidechain");
 
 // ugh CorePub
-$("[data-sidechain-src]").forEach(function(p) {
-  p.innerHTML = `<side-chain src="${p.dataset.sidechainSrc}"></side-chain>`;
-})
+var upgrade = function(element) {
+  var patch = document.createElement("side-chain");
+  patch.setAttribute("src", element.dataset.sidechainSrc);
+  element.parentNode.replaceChild(patch, element);
+}
+$("[data-sidechain-src]").forEach(upgrade);
+
+var observer = new MutationObserver(function(events) {
+  events.forEach(function(mutation) {
+    if (mutation.type == "childList") {
+      var added = Array.from(mutation.addedNodes).filter(n => n.dataset.sidechainSrc);
+      added.forEach(upgrade);
+    }
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
 
 // handle events from the inner frame
 var onMessage = Sidechain.matchMessage({
