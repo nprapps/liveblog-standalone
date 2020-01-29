ddc874d7bce316f81df1e576e37673e8cd8e7028
thomaswilburn <twilburn@npr.org>
2020-01-29 08:57:38 -0600

    Add URL flag options

--- src/js/flags.js
+++ src/js/flags.js
@@ -1,5 +1,19 @@
 var flags = {
   notifications: false
-}
+};
 
-module.export = flags;
+var search = new URL(window.location).searchParams;
+
+var boolean = ["notifications"];
+var value = ["refresh"];
+
+boolean.forEach(function(key) {
+  if (search.get(key)) flags[key] = true;
+});
+
+value.forEach(function(key) {
+  var v = search.get(key);
+  if (v) flags[key] = v;
+});
+
+module.exports = flags;
