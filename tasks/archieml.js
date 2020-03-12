6ab9e4f165a6b81d49f22b51d42438f4d8206110
thomaswilburn <twilburn@npr.org>
2020-03-12 14:10:10 -0500

    Fixes #63
    Creates a backup document during a successful run and uses that if validation fails on following builds.

--- tasks/archieml.js
+++ tasks/archieml.js
@@ -7,6 +7,13 @@ Anything that has a .txt extension in /data will be loaded
 
 var path = require("path");
 var betty = require("@nprapps/betty");
+var moment = require("moment-timezone");
+moment.fn.zoneName = function() {
+  var z = this.zoneAbbr();
+  return z.replace(/[SD]/, "");
+}
+
+var timezone = "America/New_York";
 
 module.exports = function(grunt) {
 
@@ -23,7 +30,17 @@ module.exports = function(grunt) {
       var contents = grunt.file.read(f);
 
       var parsed = betty.parse(contents, {
-        onFieldName: t => t[0].toLowerCase() + t.slice(1)
+        onFieldName: t => t[0].toLowerCase() + t.slice(1),
+        onValue: function(v) {
+          if (typeof v != "string") return v;
+          if (v == "true" || v == "false") {
+            return v != "false";
+          }
+          if (v.match(/^\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}.\d+Z$/)) {
+            return moment(v, moment.ISO_8601).tz(timezone);
+          }
+          return v;
+        }
       });
       grunt.data.archieml[name] = parsed;
     });
--- tasks/validate.js
+++ tasks/validate.js
@@ -1,13 +1,6 @@
 // runs the liveblog ArchieML through a series of tests, fails if they don't pass
 
 var { typogrify } = require("typogr");
-var moment = require("moment-timezone");
-moment.fn.zoneName = function() {
-  var z = this.zoneAbbr();
-  return z.replace(/[SD]/, "");
-}
-
-var timezone = "America/New_York";
 
 module.exports = function(grunt) {
 
@@ -15,61 +8,70 @@ module.exports = function(grunt) {
 
     grunt.task.requires("archieml");
 
-    var slugs = new Set();
+    var { liveblog, backup } = grunt.data.archieml;
 
-    var { config, posts } = grunt.data.archieml.liveblog;
+    try {
 
-    if (!config || !posts) {
-      grunt.fail.fatal("Missing config or posts section!");
-    }
+      var slugs = new Set();
 
-    var fail = function(error, data) {
-      if (data) console.log(JSON.stringify(data, null, 2));
-      grunt.fail.fatal(error);
-    }
+      var { config, posts } = liveblog;
 
-    for (var i = 0; i < posts.length; i++) {
-      var post = posts[i];
-      post.index = i;
-      // check for required non-empty fields
-      var fields = ["headline", "slug", "text", "author", "published"];
-      for (var required of fields) {
-        if (!required in post) {
-          fail(`Missing field ${required}!`, post);
+      if (!config || !posts) {
+        throw "Missing config or posts section!";
+      }
+
+      for (var i = 0; i < posts.length; i++) {
+        var post = posts[i];
+        post.index = i;
+        // check for required non-empty fields
+        var fields = ["headline", "slug", "text", "author", "published"];
+        for (var required of fields) {
+          if (!required in post) {
+            throw `Missing field ${required} for ${post.slug || post.headline || "#" + i}!`;
+          }
+          // if (!post[required].trim()) {
+          //   fail(`Field ${required} is empty`, post);
+          // }
         }
-        // if (!post[required].trim()) {
-        //   fail(`Field ${required} is empty`, post);
-        // }
+
+        // make sure the multiline fields didn't absorb other fields
+        if (post.headline.match(/text::|slug:/i)) {
+          throw `Post "${post.headline.split("\n").shift()}" is probably missing a ::headline tag`;
+        }
+        if (post.text.match(/headline::|slug:/i)) {
+          throw `Post ${post.slug} is probably missing a ::text tag`;
+        }
+
+        // no duplicate slugs
+        if (slugs.has(post.slug)) {
+          throw `Duplicate slug: ${post.slug}`;
+        }
+        slugs.add(post.slug);
+
+        // create and check dates
+        // only do this one time through the template
+        var { published } = post;
+        if (typeof published == "string") {
+          throw `Publish date value for ${post.slug} couldn't be parsed`;
+        }
       }
 
-      // make sure the multiline fields didn't absorb other fields
-      if (post.headline.match(/text::|slug:/i)) {
-        fail(`Post "${post.headline.split("\n").shift()}" is probably missing a ::headline tag`);
-      }
-      if (post.text.match(/headline::|slug:/i)) {
-        fail(`Post ${post.slug} is probably missing a ::text tag`);
-      }
+      // make a copy of the document
+      grunt.file.copy("data/liveblog.docs.txt", "data/backup.docs.txt");
 
-      // no duplicate slugs
-      if (slugs.has(post.slug)) {
-        fail(`Duplicate slug: ${post.slug}`);
-      }
-      slugs.add(post.slug);
-
-      // create and check dates
-      // only do this one time through the template
-      var { published } = post;
-      published = published.trim();
-      if (!published || published == "false") {
-        post.published = false;
+    } catch (err) {
+      console.log(`Liveblog parse error: ${err}`);
+      if (backup) {
+        console.log(`Restoring from last known good copy`);
+        grunt.data.archieml.liveblog = backup;
       } else {
-        var parsed = moment(published, moment.ISO_8601).tz(timezone);
-        if (!parsed.isValid()) {
-          fail(`Date value "${published}" on ${post.slug} is not a valid ISO8601 date.`)
-        }
-        post.published = parsed;
+        grunt.fail.fatal("No backup found, unable to continue.")
       }
-      if (post.published) {
+    }
+
+    // process post text
+    grunt.data.archieml.liveblog.posts.forEach(function(post) {
+      if (post.published && post.published.format) {
         post.timeString = post.published.format("h:mm a zz").replace(/\b([ap])m\b/, "$1.m.");
         post.dateString = post.published.format("MMM. D, YYYY");
         if (post.published.isAfter()) {
@@ -88,7 +90,7 @@ module.exports = function(grunt) {
       post.text = post.text.replace(/<image-embed[\s\S]+?src="(.*?)"[\s\S]+?<\/image-embed>/g, function(all, src) {
         return all + `<noscript><img loading="lazy" src="${src}" alt=""></noscript>`
       });
-    }
+    });
 
 
   });
