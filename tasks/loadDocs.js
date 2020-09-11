710fdea854e1ebbe13b24f4afa8fbb8f03c8a27c
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-09-11 15:34:54 -0700

    prototype of allowing basic blockquotes

--- tasks/loadDocs.js
+++ tasks/loadDocs.js
@@ -1,15 +1,15 @@
-var { google } = require("googleapis");
-var async = require("async");
-var os = require("os");
-var path = require("path");
-var { authenticate } = require("./googleauth");
+var { google } = require('googleapis');
+var async = require('async');
+var os = require('os');
+var path = require('path');
+var { authenticate } = require('./googleauth');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.registerTask(
-    "docs",
-    "Load Google Docs into the data folder",
-    function() {
-      var config = grunt.file.readJSON("project.json");
+    'docs',
+    'Load Google Docs into the data folder',
+    function () {
+      var config = grunt.file.readJSON('project.json');
       var auth = null;
       try {
         auth = authenticate();
@@ -22,13 +22,15 @@ module.exports = function(grunt) {
 
       var done = this.async();
 
-      var docs = google.docs({ auth, version: "v1" }).documents;
+      var docs = google.docs({ auth, version: 'v1' }).documents;
 
       var formatters = {
         link: (text, style) => `[${text}](${style.link.url})`,
         bold: text => `<b>${text}</b>`,
         italic: text => `<i>${text}</i>`
       };
+
+      var isNewLine = (paragraph) => paragraph.elements.length == 1 && paragraph.elements[0].textRun.content == '\n';
 
       /*
        * Large document sets may hit rate limits; you can find details on your quota at:
@@ -38,57 +40,74 @@ module.exports = function(grunt) {
       async.eachLimit(
         Object.keys(config.docs),
         2, // adjust this up or down based on rate limiting
-        async function(key) {
+        async function (key) {
           var documentId = config.docs[key];
-          var suggestionsViewMode = "PREVIEW_WITHOUT_SUGGESTIONS";
+          var suggestionsViewMode = 'PREVIEW_WITHOUT_SUGGESTIONS';
           var docResponse = await docs.get({ documentId, suggestionsViewMode });
           console.log(`Got document response for ${key}`);
-          var name = key + ".docs.txt";
+          var name = key + '.docs.txt';
           var body = docResponse.data.body.content;
-          var text = "";
+          var text = '';
 
           var lists = docResponse.data.lists;
+          var wasBlockQuote = false;
 
-          body.forEach(function(block) {
+          body.forEach(function (block, index) {
             if (!block.paragraph) return;
+            if (isNewLine(block.paragraph)) return;
             if (block.paragraph.bullet) {
               var list = lists[block.paragraph.bullet.listId];
               var level = block.paragraph.bullet.nestingLevel || 0;
               var style = list.listProperties.nestingLevels[level];
-              var bullet = "- ";
+              var bullet = '- ';
               if (style) {
-                if (style.glyphType == "DECIMAL") {
-                  bullet = "1. ";
+                if (style.glyphType == 'DECIMAL') {
+                  bullet = '1. ';
                 }
               }
-              var indent = "  ".repeat(level);
+              var indent = '  '.repeat(level);
               text += indent + bullet;
             }
-            block.paragraph.elements.forEach(function(element) {
-              // console.log(element);
+
+            var isBlockQuote =
+              block.paragraph.paragraphStyle.indentStart &&
+              block.paragraph.paragraphStyle.indentFirstLine.magnitude;
+            if (isBlockQuote && !wasBlockQuote) {
+              text += '<blockquote>\n';
+            } else if (!isBlockQuote && wasBlockQuote) {
+              text += '</blockquote>';
+            }
+
+            block.paragraph.elements.forEach(function (element, index) {
               if (!element.textRun) return;
               var { content, textStyle } = element.textRun;
-              if (content.trim())
+              if (content.replace(/[ \t\r]+/g, ''))
                 for (var f in formatters) {
                   if (textStyle[f]) {
                     var [_, before, inside, after] = content.match(
                       /^(\s*)(.*?)(\s*)$/
                     );
-                    content = before + formatters[f](inside, textStyle) + after;
+                    content =
+                      before + formatters[f](inside, textStyle) + after;
                   }
                 }
               text += content;
             });
+            wasBlockQuote = isBlockQuote;
           });
 
-          text = text.replace(/\x0b/g, "\n");
+          if (wasBlockQuote) {
+            text += '</blockquote>';
+          }
+          text = text.replace(/\x0b/g, '\n');
+          text = text.replace(/\n+/g, '\n\n');
 
           console.log(`Writing document as data/${name}`);
-          grunt.file.write(path.join("data", name), text);
+          grunt.file.write(path.join('data', name), text);
         },
-        function(err) {
+        function (err) {
           if (err) {
-            console.log("Uncaught error in docs:");
+            console.log('Uncaught error in docs:');
             console.log(err);
           }
           done();
